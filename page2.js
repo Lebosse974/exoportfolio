fbbbb96b98c3e4147fb81bef13995e1af555bd3c
Romain Equixor <r.equixor@gmail.com>
2022-03-20 13:20:13 +0400

    test js

--- page2.js
+++ page2.js
@@ -1,1 +1,30 @@
-console.log("ho")
+//console.log("ho")
+
+let yo = document.getElementById('2');
+let yoo = document.getElementById('3');
+
+
+function fu(){
+
+    yo.src = "../img/angry.png"
+}
+
+function fuout(){
+
+    yo.src = "../img/angryBird.PNG"
+
+}
+
+function fu1(){
+
+    yoo.src="../img/tenor.gif"
+
+}
+
+function fu1out(){
+    yoo.src="../img/sun in four.PNG"
+}
+
+
+
+
